abf23716d2f2a85e6f9ffb0f6a8dd056da70b8d2
Mofizul <mofizul21@gmail.com>
2024-07-01 09:54:55 +0600

    Removed access from mailtrap.php

--- includes/mailtrap.php
+++ includes/mailtrap.php
@@ -4,7 +4,7 @@ function mailtrap($phpmailer) {
     $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
     $phpmailer->SMTPAuth = true;
     $phpmailer->Port = 2525;
-    $phpmailer->Username = '3d2c5a8fc8e8e0';
-    $phpmailer->Password = '2a62879f112234';
+    $phpmailer->Username = '#';
+    $phpmailer->Password = '#';
 }
 add_action('phpmailer_init', 'mailtrap');
